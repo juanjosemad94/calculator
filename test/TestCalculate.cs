3d0022a19b4ddac1f947a9ebf6d78417c61e3899
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2022-06-20 22:36:50 +0200

    17 add unit tests (#18)
    * added test project
    
    * corrected indentation in yml file
    
    * yml correction for test step
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml

---
+++ test/TestCalculate.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+
+namespace test;
+
+public class TestCalculate
+{
+    [Fact]
+    public void Test_Calculate_Golden_Ratio()
+    {
+        var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        var response = GoldenRatioCalculator.Calculate.CalculateGoldenRatio(new DefaultHttpRequest(new DefaultHttpContext()), "720.0", logger);
+        response.Wait();
+
+        Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        var result = (OkObjectResult)response.Result;
+        var gr = new GoldenRatioCalculator.GoldenRatio { Ab = 720.0M, A =  444.99381953028430160692212608M, B = 275.02708252798782546781342774M }.ToString();
+
+        Assert.Equal(gr,  result.Value);
+    }
+}
