a0a881fe84177696f00f1231429a2ae121110e93
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2022-06-20 14:34:41 +0200

    added structured thunder tests. (#16)
    added error handling code.

--- src/Calculate.cs
+++ src/Calculate.cs
@@ -20,6 +20,7 @@ namespace GoldenRatioCalculator
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "input", In = ParameterLocation.Path, Required = true, Type = typeof(GoldenRatio), Summary = "The value to calculate the golden ratio from", Description = "The value to be used as the base for calculating the golden ratio.", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(GoldenRatio), Summary = "The calculated golden ratio values", Description = "This returns golden ratio values a and b.")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid input supplied", Description = "Invalid input supplied")]
 
         public static async Task<IActionResult> CalculateGoldenRatio(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/CalculateGoldenRatio/{input}")] HttpRequest req,
@@ -28,13 +29,16 @@ namespace GoldenRatioCalculator
         {
             log.LogInformation("C# HTTP trigger calculate function processed a request.");
 
-            decimal.TryParse(input, out decimal ab);
-
-            decimal a = ab / 1.618M;
-
-            decimal b = a / 1.618M;
-
-            return new OkObjectResult(new GoldenRatio { Ab = ab, A = a, B = b }.ToString());
+            if (decimal.TryParse(input, out decimal ab))
+            {
+                decimal a = ab / 1.618M;
+                decimal b = a / 1.618M;
+                return new OkObjectResult(new GoldenRatio { Ab = ab, A = a, B = b }.ToString());
+            }
+            else
+            {
+                return new BadRequestObjectResult($"Invalid input supplied");
+            }
         }
     }
 }
